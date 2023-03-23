1043f1cec67578c7047863363714be339ce23ba8
洪金敏 <jonny.hong91@gmail.com>
2023-03-24 02:21:51 +0800

    上行协议调通

--- zpack/pbmessage.go
+++ zpack/pbmessage.go
@@ -1,29 +1,10 @@
 package zpack
 
-import (
-	"bytes"
-	"encoding/binary"
-	"fmt"
-)
-
-type PBAsyncMessage struct {
-	Cmd  uint32
-	Body []byte
-}
-
 type PBMessage struct {
 	Message
 }
 
 // GetData 获取消息内容
 func (msg *PBMessage) GetData() []byte {
-	//byte[]内容反序列化 成 PBAsyncMessage对象
-	dataBuff := bytes.NewReader(msg.Data)
-	pbMsg := &PBAsyncMessage{}
-	err := binary.Read(dataBuff, binary.BigEndian, &pbMsg)
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-	return pbMsg.Body
+	return msg.Message.GetData()
 }
